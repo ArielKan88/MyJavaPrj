985776c22f770a4a284116575409d866804ba53d
ArielKan88 <36340477+ArielKan88@users.noreply.github.com>
2018-08-16 22:07:36 +0300

    Revert "PapayaG test project fixed"
    This reverts commit f5c257b49e40f62e9d2c08f06bd903e6724abaf7.

--- papaya-test-master/dependencies/python/solver.py
+++
@@ -1,4 +0,0 @@
-flatten = lambda l: [item for sublist in l for item in sublist]
-
-def solve(dependencies):
-    return flatten(dependencies)
--- papaya-test-master/dependencies/python/solvertest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from solver import *
-
-class SolverTest(unittest.TestCase):
-    def test_dependencies_resolved(self):
-        self.assertEqual(solve([["a", "b"], ["b", "c"]]), ["c", "b", "a"])
-
-if __name__ == '__main__':
-    unittest.main()
